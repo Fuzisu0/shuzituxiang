5113a33d5c48b22da63e2e50b7ede91cb3eb2a0d
Fan Zhisheng <f18795513456@126.com>
2024-12-23 13:50:44 +0800

    Add files via upload

---
+++ myRGBToGray.m
@@ -0,0 +1,13 @@
+function grayImg = myRGBToGray(image)
+    % 提取红色通道
+    r = image(:,:,1);
+    
+    % 提取绿色通道
+    g = image(:,:,2);
+    
+    % 提取蓝色通道
+    b = image(:,:,3);
+    
+    % 使用NTSC标准将RGB图像转换为灰度图像
+    grayImg = 0.299*r + 0.587*g + 0.114*b;
+end
